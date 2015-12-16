17ff5dcb28363fbf1a3249d6ea794fb5e91c23b6
brit13-meet <brit13@meet.mit.edu>
2015-12-16 19:42:17 +0200

    last

--- game.py
+++ game.py
@@ -1,14 +1,18 @@
 import meet
 import random 
+
 num_cells=0
 colors=["purple",'blue','green','pink']
 cells=[]
-balls1={'radius':10,'x':88,'y':3 , 'dx' : 0 , 'dy' : 0, 'color':'yellow'}
+balls1={'radius':25,'x':0,'y':-220 , 'dx' : 0 , 'dy' : 0, 'color':'yellow'}
 circle1 = meet.create_cell(balls1)
 cells.append(circle1)
+square_size=90
 
 while num_cells<15:
-	balls={'radius':random.randint(1,11) ,'x':random.randint(-200,100),'y':random.randint(-200,100) ,'dx':random.randint(-1,1),'dy':random.randint(-1,1),'color':random.choice(colors)}
+	balls={'radius':random.randint(0
+,80) ,'x':random.randint(-200+square_size,200),'y':random.randint(-180
++square_size,180) ,'dx':random.uniform(-1,1),'dy':random.uniform(-1,1),'color':random.choice(colors)}
 
 	num_cells+=1
 	circle = meet.create_cell(balls)
@@ -45,10 +49,13 @@ while True:
 			c=cell2.xcor()
 			d=cell2.ycor()
 			r2=cell2.get_radius()
-			if((a-c)**2 + (b-d)**2)**0.5 <= (r+r2):
+			if((a-c)**2 + (b-d)**2)**0.5 <= (r+r2)  :
 				if(r>r2):
 					cell2.goto(meet.get_random_x(),meet.get_random_y())
-					circle1.set_radius(r+0.5*r2)
+					circle1.set_radius(r+0.25*r2)
 				else:
-					circle1.bye()
+					exit()
+
+
+
 meet.mainloop()
